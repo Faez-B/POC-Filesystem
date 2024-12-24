664803769927cdcd990934375bb866cd382ca478
Faez-B <faezbhatti@gmail.com>
2024-12-24 17:17:38 +0100

    ✨: maker bundle + PicoCSS ✨: home route

--- assets/app.js
+++ assets/app.js
@@ -4,6 +4,9 @@
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
+import '@picocss/pico/css/pico.min.css';
+import '@picocss/pico';
+
 import './styles/app.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
