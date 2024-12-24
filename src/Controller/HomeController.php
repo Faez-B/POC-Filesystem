075d64cb0bf75b94922aaa617b37e03b86db7554
Faez-B <faezbhatti@gmail.com>
2024-12-24 21:32:53 +0100

    ✨: created a form to upload a file

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,27 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\ProductType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
-    {
-        return $this->render('home/index.html.twig');
+    public function index(
+        Request $request,
+    ): Response {
+        $form = $this->createForm(ProductType::class);
+        
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {         
+            // Do something with the uploaded file
+        }
+
+        return $this->render('home/index.html.twig', [
+            'form' => $form,
+        ]);
     }
 }
